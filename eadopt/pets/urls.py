9e3fc8c0ac23cce100275a1c9af45c457269069d
Felipe Caetano Silva <felipecaetanosilva@hotmail.com>
2018-06-25 19:50:12 -0300

    Cria Pets

--- eadopt/pets/urls.py
+++ eadopt/pets/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='pets_index'),
+    url(r'^index/$', views.index, name='pets_index'),
     url(r'^novo/$', views.novo, name='pet_novo'),
     url(r'^criar/$', views.criar, name='pet_criar'),
     # url(r'^editar/$', views.editar, name='usuario_editar'),
--- eadopt/pets/views.py
+++ eadopt/pets/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from usuarios.models import Usuario, PF, PJ
+from pets.models import Pet
+from usuarios.models import Usuario
 from django.contrib import messages
 from django.shortcuts import render, redirect
 from eadopt.mongo import conectar_mongo
@@ -7,25 +8,36 @@ from bson.objectid import ObjectId
 import django.utils.formats as fmt
 
 def index(request):
-    return render(request, 'pets_do_usuario.html')
+    pets = list(Pet.objects.filter(dono_id=request.session['usuario_id']))
+    for pet in pets:
+        print (pet.nome)
+    print("OIiiiii")
+    return render(request, 'pets_do_usuario.html', { "pets":pets })
 
 def novo(request):
-    return render(request, 'novo.html')
+    return render(request, 'novo_pet.html')
 
 
 def criar(request):
     novo_pet = preencher(request)
     novo_pet.save()
+    db = conectar_mongo()
+    sitedb = db.pets
     resultado = conectar_mongo().pets.insert_one({
         'id_postgres': novo_pet.id,
-        'nome': novo_pet.nome
+        'nome': novo_pet.nome,
+        'descricao': request.POST['descricao']
         })
     novo_pet.id_mongo = str(resultado.inserted_id)
     novo_pet.save()
-    return redirect('index')
+    print("OIiiiii")
+    return redirect('pets_index')
 
-    def preencher(request):
-        pet = Pet()
-        pet.data_nascimento = request.POST['data_nascimento']
-        pet.nome = request.POST['nome']
-        pet.especie = request.POST['especie']
+def preencher(request):
+    pet = Pet()
+    pet.dono_id = request.session['usuario_id']
+    pet.data_nascimento = request.POST['data_nascimento']
+    pet.nome = request.POST['nome']
+    pet.especie = request.POST['especie']
+
+    return pet
