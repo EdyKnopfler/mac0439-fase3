40612fb7137750789d67951a59cd41ff366dc923
Gabriela Lima <gabrielarvlima@gmail.com>
2018-06-25 18:35:47 -0300

    small fix - fiz upload da versao errada

--- eadopt/posts/models.py
+++ eadopt/posts/models.py
@@ -7,7 +7,12 @@ class Post(models.Model):
     data_hora = models.DateTimeField(auto_now_add=True)
     titulo = models.CharField(max_length=80, null=False, blank=False)
     id_mongo = models.CharField(max_length=24, null=True, blank=True)
-    arquivo = models.FileField(upload_to='midia/')
+    tem_midia = models.BooleanField()
+
+class Midia(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    binario = models.BinaryField(null=False, blank=False)
+    formato = models.CharField(max_length=11, null=False, blank=False)
 
 class MarcadoNoPost(models.Model):
     usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
