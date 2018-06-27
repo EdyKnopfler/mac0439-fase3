a44ef8c7e2b635ef3342bc7fa2eff0b41e2ed93b
Éderson Cássio <ederson_cassio@yahoo.com.br>
2018-06-27 09:46:17 -0300

    Refatoração; preenchimento dos status de requisitos; controle transacional no settings.py

--- eadopt/eadopt/settings.py
+++ eadopt/eadopt/settings.py
@@ -93,6 +93,7 @@ DATABASES = {
         'PASSWORD': 'jethrotull',
         'HOST': '127.0.0.1',
         'PORT': '5432',
+        'ATOMIC_REQUESTS': True,
     }
 }
 
--- eadopt/processos_doacao/views.py
+++ eadopt/processos_doacao/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render, redirect
 from datetime import datetime
 
-from processos_doacao.models import ProcessoDoacao
+from processos_doacao.models import ProcessoDoacao, StatusRequisito
 from anuncios_doacao.models import AnuncioDoacao
+from autenticacao.autorizacao import *
+
+
+BLOQUEIO = erro_autorizacao('somente o dono do Anúncio de Doação tem acesso a esta funcionalidade')
 
 
 def criar(request, anuncio_id):
@@ -24,5 +28,26 @@ def cancelar(request, anuncio_id):
     
 def candidatos(request, anuncio_id):
     anuncio = AnuncioDoacao.objects.get(id=anuncio_id)
+    if not dono_anuncio(request, anuncio): return BLOQUEIO
     processos = ProcessoDoacao.objects.filter(anuncio_id=anuncio_id)
     return render(request, 'processos_doacao/index.html', {'anuncio':anuncio, 'processos':processos})
+    
+    
+def requisitos(request, processo_id):
+    processo = ProcessoDoacao.objects.get(id=processo_id)
+    if not dono_anuncio(request, processo.anuncio): return BLOQUEIO
+    status_req = StatusRequisito.objects.filter(anuncio_id=processo.anuncio_id, candidato_id=processo.candidato_id)
+    return render(request, 'processos_doacao/requisitos.html', {'candidato':processo.candidato.nome, 
+            'processo_id':processo_id, 'status_req':status_req})
+    
+    
+def atualizar_requisitos(request):
+    processo = ProcessoDoacao.objects.get(id=request.POST['processo_id'])
+    if not dono_anuncio(request, processo.anuncio): return BLOQUEIO
+    status_req = StatusRequisito.objects.filter(anuncio_id=processo.anuncio_id, candidato_id=processo.candidato_id)
+    for s in status_req:
+        s.status = request.POST['status[' + str(s.id) + ']']
+        print(s.status)
+        s.save()
+    return redirect('processo_index', processo.anuncio_id)
+    
