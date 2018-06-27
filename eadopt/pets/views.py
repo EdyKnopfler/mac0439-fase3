b54b01cbeeca83a7abba98dcc7765c7e4d50ad91
Gabriela Lima <gabrielarvlima@gmail.com>
2018-06-27 15:49:58 -0300

    fotos para pets

--- eadopt/pets/views.py
+++ eadopt/pets/views.py
@@ -1,11 +1,14 @@
-from django.shortcuts import render     
-from pets.models import Pet
+from django.shortcuts import render
+from pets.models import Pet, Foto
 from usuarios.models import Usuario
 from django.contrib import messages
 from django.shortcuts import render, redirect
 from eadopt.mongo import conectar_mongo
 from bson.objectid import ObjectId
 import django.utils.formats as fmt
+from django.core.files.storage import FileSystemStorage
+import sys
+import os
 
 def index(request):
     pets = list(Pet.objects.filter(dono_id=request.session['usuario_id']))
@@ -27,6 +30,26 @@ def atualizar(request):
     pet.id = request.session["pet_id"]
     pet.id_mongo = request.session['pet_mongo_id']
     pet.save()
+    try:
+        print(request.POST)
+        print(request.FILES)
+        fotos = request.FILES.getlist('arquivo')
+        print(fotos)
+        fs = FileSystemStorage()
+        for arqv in fotos:
+            print(arqv)
+            print(arqv.name)
+            filename, file_extension =  os.path.splitext(arqv.name)
+            filename = fs.save(
+                str(request.session['usuario_id']) + "_" + str(novo_pet.id) + file_extension, arqv)
+            foto = Foto()
+            foto.pet_id = novo_pet.id
+            foto.arquivo = filename
+            foto.save()
+    except Exception:
+        e = sys.exc_info()
+        print("erro!")
+        print(e)
     conectar_mongo().pets.update_one({"_id": ObjectId(request.session['pet_mongo_id'])}, {
         "$set": {'nome':request.POST['nome'], 'descricao': request.POST['descricao']}
     })
@@ -38,11 +61,32 @@ def remover(request):
 def perfil(request, pet_id):
     pet = Pet.objects.get(id=pet_id)
     dono = Usuario.objects.get(id=pet.dono_id)
-    return render(request, 'perfil_pet.html', {"pet":pet, "dono":dono})
+    fotos = Foto.objects.filter(pet_id = pet.id)
+    return render(request, 'perfil_pet.html', {"pet":pet, "dono":dono, "fotos":fotos})
 
 def criar(request):
     novo_pet = preencher(request)
     novo_pet.save()
+    try:
+        print(request.POST)
+        print(request.FILES)
+        fotos = request.FILES.getlist('arquivo')
+        print(fotos)
+        fs = FileSystemStorage()
+        for arqv in fotos:
+            print(arqv)
+            print(arqv.name)
+            filename, file_extension =  os.path.splitext(arqv.name)
+            filename = fs.save(
+                str(request.session['usuario_id']) + "_" + str(novo_pet.id) + file_extension, arqv)
+            foto = Foto()
+            foto.pet_id = novo_pet.id
+            foto.arquivo = filename
+            foto.save()
+    except Exception:
+        e = sys.exc_info()
+        print("erro!")
+        print(e)
     db = conectar_mongo()
     sitedb = db.pets
     resultado = conectar_mongo().pets.insert_one({
@@ -52,7 +96,6 @@ def criar(request):
         })
     novo_pet.id_mongo = str(resultado.inserted_id)
     novo_pet.save()
-    print("OIiiiii")
     return redirect('pets_index')
 
 def preencher(request):
@@ -63,4 +106,3 @@ def preencher(request):
     pet.especie = request.POST['especie']
     pet.descricao = request.POST['descricao']
     return pet
-
