2219ad03081b44e7c974d5767dd1a0dfa156762a
Felipe Caetano Silva <felipecaetanosilva@hotmail.com>
2018-06-27 09:50:23 -0300

    editar

--- eadopt/eadopt/settings.py
+++ eadopt/eadopt/settings.py
@@ -89,8 +89,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'fase3',
-        'USER': 'ederson',
-        'PASSWORD': 'jethrotull',
+        'USER': 'fcaetano',
+        'PASSWORD': '235711novo',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
